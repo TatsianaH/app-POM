b971ae363771dfe0380e94a00758324164dc5988
TatsianaH <ourflsun@gmail.com>
2020-03-10 17:05:48 -0400

    create

--- test/specs/webstaurant.spec.js
+++ test/specs/webstaurant.spec.js
@@ -6,7 +6,7 @@ describe('Steel table', () => {
   it('should open home page', () => {
     browser.url('https://www.webstaurantstore.com/');
     const title = browser.getTitle();
-    expect(title).eq('WebstaurantStore');
+    expect(title).eq('WebstaurantStore: Restaurant Supplies & Foodservice Equipment');
   });
 
   it('should insert value and get redirected to Search Page', () => {
@@ -17,7 +17,13 @@ describe('Steel table', () => {
     const searchPageUrl = 'https://www.webstaurantstore.com/search/stainless-work-table.html';
     const textInH1 = 'Stainless Work Table';
     const inH1SelText = $('//h1/span[text()="Stainless Work Table"]').getText();
-      expect(browser.getUrl()).eq(searchPageUrl);
-      expect(textInH1).eq(inH1SelText);
+    expect(browser.getUrl()).eq(searchPageUrl);
+    expect(textInH1).eq(inH1SelText);
   });
+
+  it('should get list of search results from titles', () => {
+    const listResultesFromTitles = $$('//div[@class="ag-item"]//a');
+    expect(listResultesFromTitles.map(el => el.getAttribute('title')).every(el => el.includes('Table') || el.includes('Tables')));
+  });
+
 });
