018278f6a0286e555f1708da187f1660bf39f99c
TatsianaH <ourflsun@gmail.com>
2020-03-22 22:55:14 -0400

    add tests

--- test/specs/asana.spec.js
+++ test/specs/asana.spec.js
@@ -1,15 +1,24 @@
 import { expect } from 'chai';
+import axios  from 'axios';
 
 const sel = {
   h1: '//h1',
   mainMenu: '//ul[@role="group"]',
   menuItem: '//ul[@role="group"]/li',
+  whyAsanaDropDownMenu: '//div[@id="navigation__dropdown-why-asana"]',
+  solutionsDropDownMenu: 'div[@id="navigation__dropdown-solutions]',
+  resourcesDropDownMenu:'',
+  pricing: '',
 };
 
+
 const data = {
-  h1Text: 'Make more time for the work that matters most',
+  h1Text: 'Keep your team organized and connected',
   url: 'https://asana.com/',
+
 };
+
+let mainMenuItem = null;
 
 describe('Asana website', () => {
   it('should go to the main page', () => {
@@ -21,14 +30,39 @@ describe('Asana website', () => {
     expect($(sel.mainMenu).isDisplayed());
   });
 
-  it('should check main menu has length equals 4', () => {
+  it('should check the main menu has length equals 4', () => {
     console.log($$(sel.menuItem).length, '============');
+    mainMenuItem = $$(sel.menuItem);
     expect($$(sel.menuItem)).to.have.lengthOf(4);
   });
 
-  it('should check main menu has length equals 4', () => {
-    console.log($$(sel.menuItem).length, '============');
-    expect($$(sel.menuItem)).to.have.lengthOf(4);
+  it('should check the main menu items are clickable', () => {
+    for(let i = 0; i < mainMenuItem.length; i++){
+      expect(mainMenuItem[i].isClickable());
+    }
+  });
+
+  it('should check `why Asana?` is clickable', () => {
+    mainMenuItem[0].click();
+    expect($(sel.whyAsanaDropDownMenu).isDisplayed());
+  });
+
+  it('should check all links in `Why Asana?` are redirect to proper page', async () => {
+    async function checkUrl() {
+      return axios({
+        method: 'GET',
+        url: 'https://asana.com/product',
+      })
+        .then(res => {
+          return res.status;
+        })
+        .catch(err => {
+          console.log(err);
+          return err;
+        });
+    }
+    const actual = await checkUrl();
+    expect(actual).eq(200);
   });
 
 });
